16d30b80c25beb7ec6c6c83552c4b702430d9ac6
lucibrigida <54370158+lucibrigida@users.noreply.github.com>
2019-10-24 14:34:08 -0300

    adicionando um arquivo a mais
    criando a tela inicial a mais

---
+++ cadastro.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<form action="insert_table.php" method="post">
+Nome: <input type="text" name="name"><br>
+Sobrenome: <input type="text" name="lastname"><br>
+E-mail: <input type="text" name="email"><br>
+Telefone: <input type="text" name="telefone"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>

d1fdcaeb52b099bab2b5b8b67fdca3b1db328699
Jonas <54370074+Jonasjanu@users.noreply.github.com>
2019-11-22 08:45:34 -0300

    adsicão
    adsicão

---
+++ ArquivoPDF.php
@@ -0,0 +1,28 @@
+<?php
+	include ('FPDF/fpdf.php');
+
+	$pagina = 
+		"<html>
+			<body>
+				<h1>Relatório de lead cadastrado</h1>
+				<ul>
+					<li>cesar@celke.com.br</li>
+					<li>kelly@celke.com.br</li>
+					<li>atendimento@celke.com.br</li>
+				</ul>
+				<h4>http://www.celke.com.br</h5>
+			</body>
+		</html>
+		";
+
+$arquivo = "Cadastro01.pdf";
+
+$mpdf = new FPDF();
+$mpdf->WriteHTML($pagina);
+
+$mpdf->Output($arquivo, 'I');
+
+// I - Abre no navegador
+// F - Salva o arquivo no servido
+// D - Salva o arquivo no computador do usuário
+?>
